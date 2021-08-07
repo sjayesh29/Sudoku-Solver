66867028716e818eadb55a63d4084eea688bc2f4
sjayesh29 <87217650+sjayesh29@users.noreply.github.com>
2021-08-07 08:49:14 +0530

    Update sudokusolver.py

--- sudokusolver.py
+++ sudokusolver.py
@@ -4,34 +4,23 @@ from pprint import pprint
 def next_empty(puzzle):
     
     for r in range(9):
-        for c in range(9): # range(9) is 0, 1, 2, ... 8
+        for c in range(9): 
             if puzzle[r][c] == 0:
                 return r, c
 
-    return None, None  # if no spaces in the puzzle are empty (0)
+    return None, None  
 
 def is_valid(puzzle, guess, r, c):
-    # figures out whether the guess at the row/col of the puzzle is a valid guess
-    # returns True or False
-
-    # for a guess to be valid, then we need to follow the sudoku rules
-    # that number must not be repeated in the row, column, or 3x3 square that it appears in
-
-    # let's start with the row
     row_vals = puzzle[r]
     if guess in row_vals:
-        return False # if we've repeated, then our guess is not valid!
+        return False 
 
-    # now the column
-    # col_vals = []
-    # for i in range(9):
-    #     col_vals.append(puzzle[i][col])
+    
     col_vals = [puzzle[i][c] for i in range(9)]
     if guess in col_vals:
         return False
 
-    # and then the square
-    r_s = (r // 3) * 3 # 10 // 3 = 3, 5 // 3 = 1, 1 // 3 = 0
+    r_s = (r // 3) * 3 
     c_s = (c // 3) * 3
 
     for r in range(r_s, r_s + 3):
